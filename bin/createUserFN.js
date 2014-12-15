bfd1bf6c7bbd3a692467526c24daf6156b7fe06e
Sávio Lucena <saviogl@gmail.com>
2014-12-15 14:45:15 -0300

    Update createUserFN for devLogin

--- bin/createUserFN.js
+++ bin/createUserFN.js
@@ -2,11 +2,12 @@ var sget = require('sget');
 
 var userStub = {
   username: 'afrosamuray',
-  biography: 'Afro Samuray is a hero how help people!',
+  biography: 'Afro Samuray is a hero how helps people!',
   email: 'contato@albertosouza.net',
   password: '123',
   displayName: 'Afro Samuray',
-  language: 'pt-br'
+  language: 'pt-br',
+  active: true
 }
 
 module.exports = function createUser(done) {
@@ -17,18 +18,18 @@ module.exports = function createUser(done) {
   sails.log.info('-');
 
   // alows user set new user data
-  var whantsSendUserData = sget('You want choose the user data to create?. \n y or n?');
+  var whantsSendUserData = sget('Do you want to choose the user data to be created?. \n y or n?');
   // remove \n
   whantsSendUserData = whantsSendUserData.replace('\n','');
 
   if ( whantsSendUserData === 'y') {
-    userStub.displayName = sget('How are the displayName?');
+    userStub.displayName = sget('What is the displayName?');
     userStub.displayName = userStub.displayName.replace('\n','');
-    userStub.username = sget('How are the username?');
+    userStub.username = sget('What is the username?');
     userStub.username = userStub.username.replace('\n','');
-    userStub.email = sget('How are the email?');
+    userStub.email = sget('What is the email?');
     userStub.email = userStub.email.replace('\n','');
-    userStub.password = sget('How are the password?');
+    userStub.password = sget('What is the password?');
     userStub.password = userStub.password.replace('\n','');
   } else {
     sails.log.info('I will create the user: ', userStub);
