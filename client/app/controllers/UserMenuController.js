ab78bcbfc01a3c1bdd5ebe671e7135111bad0c1a
Sávio Lucena <saviogl@gmail.com>
2015-03-10 14:26:12 -0300

    new user menu

--- client/app/controllers/UserMenuController.js
+++ client/app/controllers/UserMenuController.js
@@ -2,6 +2,27 @@
   App.UserMenuController = Ember.Controller.extend({
     isVisible: false,
 
+    accountsLinks: function (){
+      var host = this.get('accountsHost');
+
+      this.setProperties({
+        accounts: host + '/accounts'
+      });
+
+    }.observes('accountsHost').on('init'),
+
+    drupalLinks: function () {
+      var host = this.get('drupalHost');
+
+      this.setProperties({
+        messages: host + '/user/',
+        friends: host + '/user/',
+        notification: host + '/user/',
+        notes: host + '/user/'
+      });
+
+    }.observes('drupalHost').on('init'),
+
     currentUser: function () {
       return App.get('currentUser');
     }.property('App.currentUser'),
@@ -16,6 +37,10 @@
       if(App.currentUser.id){
         self.set('isVisible', true);
       }
+      // Set drupal host for menu
+      self.set('drupalHost', we.configs.client.publicVars.drupal);
+      self.set('accountsHost', we.configs.server.providers.accounts);
+
       we.hooks.on("user-authenticated",function(user, done){
         self.set('isVisible', true);
         done();
