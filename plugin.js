9546e7572aef29b8602f2e9ab720112690ae82d1
albertosouza <alberto.souza.99@gmail.com>
2016-05-23 02:25:11 -0300

    add code from we-core ref wejs/we#139

---
+++ plugin.js
@@ -0,0 +1,122 @@
+/**
+ * User main file
+ */
+module.exports = function loadUserPlugin(projectPath, Plugin) {
+  var plugin = new Plugin(__dirname);
+
+  plugin.setConfigs({
+    defaultUserAvatar: projectPath + '/node_modules/we-core/files/public/images/avatars/user-avatar.png',
+    /**
+     * Field privacity settings
+     * @type {Object}
+     */
+    privacity: {
+      userFields: {
+        public: ['displayName', 'avatar', 'banner'],
+        changeable: [
+          'fullName', 'biography', 'gender', 'language', 'organization'
+        ]
+      }
+    },
+  });
+
+  plugin.setRoutes({
+    'get /account': {
+      'controller': 'auth',
+      'action': 'current',
+      'model': 'user'
+    },
+    'get /user/:userId([0-9]+)/edit/privacity': {
+      'name'          : 'user.privacity',
+      'controller'    : 'user',
+      'model'         : 'privacity',
+      'action'        : 'findUserPrivacity',
+      'titleHandler'  : 'i18n',
+      'titleI18n'     : 'Privacity',
+      'layoutName'    : 'user-layout',
+      'template'      : 'user/findUserPrivacity'
+    },
+    'post /user/:userId([0-9]+)/edit/privacity': {
+      'name'          : 'user.privacity',
+      'controller'    : 'user',
+      'model'         : 'privacity',
+      'action'        : 'findUserPrivacity',
+      'titleHandler'  : 'i18n',
+      'titleI18n'     : 'Privacity',
+      'layoutName'    : 'user-layout',
+      'template'      : 'user/findUserPrivacity'
+    },
+  });
+
+  plugin.setResource({
+    'name': 'user',
+    'findOne': {
+      'metatagHandler': 'userFindOne'
+    },
+    'findAll': {
+      'search': {
+        'q': {
+          'parser': 'userSearchQuery',
+          'target': {
+            'type': 'field',
+            'field': 'id'
+          }
+        }
+      }
+    }
+  });
+
+  plugin.setResource({
+    'namePrefix': 'admin.',
+    'name': 'user',
+    'namespace': '/admin',
+    'templateFolderPrefix': 'admin/',
+    'findAll': {
+      'search': {
+        'id':  {
+          'parser': 'equal',
+          'target': {
+            'type': 'field',
+            'field': 'id'
+          }
+        },
+        'email':  {
+          'parser': 'equal',
+          'target': {
+            'type': 'field',
+            'model': 'user',
+            'field': 'email'
+          }
+        },
+        'fullName':  {
+          'parser': 'contains',
+          'target': {
+            'type': 'field',
+            'model': 'user',
+            'field': 'fullName'
+          }
+        }
+      }
+    }
+  });
+
+  plugin.events.on('we:express:set:params', function(data) {
+    // user pre-loader
+    data.express.param('userId', function (req, res, next, id) {
+      if (!/^\d+$/.exec(String(id))) return res.notFound();
+      data.we.db.models.user.findById(id)
+      .then(function (user) {
+        if (!user) return res.notFound();
+        res.locals.user = user;
+        // set user context if userId is the first param
+        if (Object.keys(req.params)[0] == 'userId'){
+          res.locals.widgetContext = 'user-' + id;
+        }
+
+        next();
+      });
+    });
+  });
+
+  return plugin;
+};
